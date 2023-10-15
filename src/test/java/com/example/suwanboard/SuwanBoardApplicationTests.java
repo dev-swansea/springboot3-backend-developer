aaa1dc32fc9cf74d6db15e82ef8d3d1e3847ea12
swansea <swansea2013@naver.com>
2023-10-15 17:32:15 +0900

    9장 jwt 토큰 생성

--- src/test/java/com/example/suwanboard/SuwanBoardApplicationTests.java
+++ src/test/java/com/example/suwanboard/SuwanBoardApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.suwanboard;
 
+import com.example.suwanboard.config.jwt.JwtProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
